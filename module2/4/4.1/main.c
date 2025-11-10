f0fa79426caba89a11443ea31ce1356f7bc2c3ef
AnnaUzh <anytik0509@gmail.com>
2025-11-10 15:04:27 +0700

    4.1

---
+++ module2/4/4.1/main.c
@@ -0,0 +1,62 @@
+#include "struct.h"
+
+void main() {
+  setlocale(LC_ALL, "Russian");
+
+  PhoneBook phone_book;
+  phone_book.count = 0;
+
+  while (1) {
+    printf("Выберите функцию для работы с таблицей\n");
+    printf("1 - добавление контакта\n");
+    printf("2 - редактирование контакта\n");
+    printf("3 - удаление контакта;\n");
+    printf("4 - просмотра телефонной книги\n");
+    printf("5 - просмотр контакта \n");
+    printf("0 - завершить работу\n");
+
+    char answer[10], ch;
+    int c = 0;
+    do {
+      ch = getchar();
+      answer[c] = ch;
+      c++;
+    } while (ch != '\n');
+    c--;
+    answer[c] = '\0';
+
+    if (c != 1) {
+      printf("Введите корректную команду\n");
+      continue;
+    }
+    if (answer[0] - '0' < 0 || answer[0] - '0' > 5) {
+      printf("Введите корректную команду\n");
+      continue;
+    }
+    struct student *sorted;
+    int ans = answer[0] - '0';
+    switch (ans) {
+    case 1:
+      addContact(&phone_book);
+      break;
+    case 2:
+      uint32_t id2 = search(&phone_book);
+      edit_contact(&phone_book, id2);
+      break;
+    case 3:
+      uint32_t id3 = search(&phone_book);
+      delete_contact(&phone_book, id3);
+      break;
+    case 4:
+      print_all_contacts(&phone_book);
+      break;
+    case 5:
+      uint32_t id5 = search(&phone_book);
+      printContact(id5, &phone_book);
+      break;
+    case 0:
+      return;
+      break;
+    }
+  }
+}
---
+++ module2/4/4.1/struct.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include<stdio.h>
+#include<locale.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <time.h>
+#include <ctype.h>
+#define MAX_CONTACTS 1000
+#define MAX_LENGTH 100
+#define MAX_EMAILS 10
+#define MAX_SOCIALS 20
+#define MAX_MESSENGERS 20
+#define MAX_PHONES 5
+typedef struct Contact Contact;
+// Основная структура контакта
+struct Contact{
+    uint32_t id;
+    char last_name[MAX_LENGTH];    // Фамилия (обязательное поле)
+    char first_name[MAX_LENGTH];   // Имя (обязательное поле)
+    char middle_name[MAX_LENGTH];  // Отчество
+    char company[MAX_LENGTH];      // Место работы
+    char position[MAX_LENGTH];     // Должность
+    char phone[MAX_PHONES][MAX_LENGTH]; 
+    int phone_count;
+    char email[MAX_EMAILS][MAX_LENGTH];
+    int email_count;
+    char social_media[MAX_SOCIALS][MAX_LENGTH];
+    int social_media_count;
+    char messenger[MAX_MESSENGERS][MAX_LENGTH];
+    int messenger_count;
+    Contact *next;
+    Contact *prev;
+};
+
+// Структура для работы с телефонной книгой
+typedef struct {
+    Contact contacts[MAX_CONTACTS]; // Массив контактов
+    int count;                      // Текущее количество контактов
+} PhoneBook;
+
+int is_empty_or_whitespace(const char *str);
+void printContact(const uint32_t contact_id,const PhoneBook *phone_book);
+int addContact(PhoneBook *phone_book);
+void print_all_contacts(const PhoneBook *phone_book);
+uint32_t name_search(PhoneBook *phonebook, char target[], int len);
+uint32_t last_name_search(PhoneBook *phonebook, char target[], int len);
+Contact delete_contact(PhoneBook *phone_book, uint32_t contact_id);
+uint32_t search(PhoneBook *phone_book);
+void read_string_with_default(const char *prompt, char *buffer, const char *default_value);
+int edit_contact(PhoneBook *phone_book, uint32_t contact_id);
+int is_id_exists(const PhoneBook *phone_book, int id);
+uint32_t generate_unique_id(PhoneBook *phone_book);
