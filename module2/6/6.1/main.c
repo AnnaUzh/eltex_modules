7e4026ef89833875da08773fd2c68fe43fd778a9
AnnaUzh <anytik0509@gmail.com>
2025-12-02 18:42:57 +0700

    6.1

---
+++ module2/6/6.1/main.c
@@ -0,0 +1,65 @@
+#include "struct.h"
+
+void main() {
+  setlocale(LC_ALL, "Russian");
+
+  PhoneBook phone_book;
+  init_phonebook(&phone_book);
+
+  while (1) {
+    printf("Выберите функцию для работы с таблицей\n");
+    printf("1 - добавление контакта\n");
+    printf("2 - редактирование контакта\n");
+    printf("3 - удаление контакта;\n");
+    printf("4 - просмотра телефонной книги\n");
+    printf("5 - просмотр контакта \n");
+    printf("0 - завершить работу\n");
+
+    char answer[10];
+    if (fgets(answer, sizeof(answer), stdin) == NULL) {
+      continue;
+    }
+    
+    size_t len = strlen(answer);
+    if (len > 0 && answer[len-1] == '\n') {
+      answer[len-1] = '\0';
+      len--;
+    }
+
+    if (len != 1) {
+      printf("Введите корректную команду\n");
+      continue;
+    }
+    if (answer[0] < '0' || answer[0] > '5') {
+      printf("Введите корректную команду\n");
+      continue;
+    }
+    
+    struct student *sorted;
+    int ans = answer[0] - '0';
+    switch (ans) {
+    case 1:
+      addContact(&phone_book);
+      sort_by_last_name(&phone_book);
+      break;
+    case 2:
+      uint32_t id2 = search(&phone_book);
+      edit_contact(&phone_book, id2);
+      sort_by_last_name(&phone_book);
+      break;
+    case 3:
+      uint32_t id3 = search(&phone_book);
+      delete_contact(&phone_book, id3);
+      break;
+    case 4:
+      print_all_contacts(&phone_book);
+      break;
+    case 5:
+      uint32_t id5 = search(&phone_book);
+      break;
+    case 0:
+      return;
+      break;
+    }
+  }
+}
