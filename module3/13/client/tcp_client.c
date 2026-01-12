8d17183e54c14e38458b3c5afe716fd5aad8c2f1
AnnaUzh <anytik0509@gmail.com>
2026-01-12 23:21:09 +0700

    13

--- module3/13/client/tcp_client.c
+++ module3/13/client/tcp_client.c
@@ -14,7 +14,6 @@ void error(const char *msg) {
     exit(0);
 }
 
-// Функция для отправки файла
 void send_file(int sock, const char* filename) {
     FILE *file = fopen(filename, "rb");
     if (!file) {
@@ -23,22 +22,18 @@ void send_file(int sock, const char* filename) {
         return;
     }
     
-    // Получаем размер файла
     fseek(file, 0, SEEK_END);
     long file_size = ftell(file);
     fseek(file, 0, SEEK_SET);
     
-    // Отправляем размер файла
     send(sock, &file_size, sizeof(file_size), 0);
     
-    // Ждем подтверждения от сервера
     char ack[10];
     if (recv(sock, ack, sizeof(ack), 0) <= 0) {
         fclose(file);
         return;
     }
     
-    // Отправляем файл
     char buffer[4096];
     size_t bytes_read;
     long total_sent = 0;
@@ -67,39 +62,31 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
     
-    // извлечение порта
     portno = atoi(argv[2]);
     
-    // Шаг 1 - создание сокета
     my_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (my_sock < 0)
         error("ERROR opening socket");
     
-    // извлечение хоста
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
     
-    // заполнение структуры serv_addr
     bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,
           server->h_length);
     
-    // установка порта
     serv_addr.sin_port = htons(portno);
     
-    // Шаг 2 - установка соединения
     if (connect(my_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
     
     printf("Connected to server\n");
     
-    // Шаг 3 - чтение и передача сообщений
     while (1) {
-        // Получаем сообщение от сервера
         bzero(buff, sizeof(buff));
         n = recv(my_sock, &buff[0], sizeof(buff) - 1, 0);
         
@@ -111,7 +98,6 @@ int main(int argc, char *argv[]) {
         buff[n] = 0;
         printf("Server: %s", buff);
         
-        // Читаем пользовательский ввод
         printf("Client: ");
         fflush(stdout);
         
@@ -120,12 +106,10 @@ int main(int argc, char *argv[]) {
             break;
         }
         
-        // Убираем символ новой строки
         if (buff[strlen(buff)-1] == '\n') {
             buff[strlen(buff)-1] = '\0';
         }
         
-        // Проверка на "quit"
         if (strcmp(&buff[0], "quit") == 0) {
             printf("Exit...\n");
             send(my_sock, "quit", 4, 0);
@@ -133,15 +117,12 @@ int main(int argc, char *argv[]) {
             return 0;
         }
         
-        // Проверка на команду отправки файла
         if (strncmp(&buff[0], "sendfile ", 9) == 0) {
             char filename[256];
             strcpy(filename, &buff[9]);
             
-            // Отправляем команду серверу
             send(my_sock, &buff[0], strlen(&buff[0]), 0);
             
-            // Ждем подтверждения от сервера
             bzero(buff, sizeof(buff));
             n = recv(my_sock, &buff[0], sizeof(buff) - 1, 0);
             if (n > 0) {
@@ -149,12 +130,10 @@ int main(int argc, char *argv[]) {
                 printf("Server: %s", buff);
             }
             
-            // Отправляем файл
             send_file(my_sock, filename);
             continue;
         }
         
-        // Отправляем обычное сообщение серверу
         send(my_sock, &buff[0], strlen(&buff[0]), 0);
     }
     
--- module3/13/server/tcp_server.c
+++ module3/13/server/tcp_server.c
@@ -9,17 +9,13 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 
-// функция обслуживания подключившихся пользователей
 void dostuff(int);
-// функция обработки ошибок
 void error(const char *msg) {
     perror(msg);
     exit(1);
 }
-// количество активных пользователей
 int nclients = 0;
 
-// печать количества активных пользователей
 void printusers() {
     if (nclients) {
         printf("%d user on-line\n", nclients);
@@ -28,13 +24,11 @@ void printusers() {
     }
 }
 
-// функция обработки данных
 int add_func(int a, int b) { return a + b; }
 int sub_func(int a, int b) { return a - b; }
 int mul_func(int a, int b) { return a * b; }
 int div_func(int a, int b) { return a / b; }
 
-// функция для приема файла
 void receive_file(int sock, const char* filename) {
     FILE *file = fopen(filename, "wb");
     if (!file) {
@@ -46,14 +40,13 @@ void receive_file(int sock, const char* filename) {
     char buffer[4096];
     ssize_t bytes_received;
     long file_size;
-    
-    // Получаем размер файла
+
     if (recv(sock, &file_size, sizeof(file_size), 0) <= 0) {
         fclose(file);
         return;
     }
     
-    send(sock, "READY", 5, 0);  // Подтверждаем готовность принять файл
+    send(sock, "READY", 5, 0);
     
     long total_received = 0;
     while (total_received < file_size) {
@@ -70,48 +63,42 @@ void receive_file(int sock, const char* filename) {
 }
 
 int main(int argc, char *argv[]) {
-    int sockfd, newsockfd; // дескрипторы сокетов
-    int portno;            // номер порта
-    int pid;               // id номер потока
-    socklen_t clilen; // размер адреса клиента типа socklen_t
-    struct sockaddr_in serv_addr, cli_addr; // структура сокета сервера и клиента
+    int sockfd, newsockfd;
+    int portno;
+    int pid;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
     
     printf("TCP SERVER DEMO\n");
     
-    // ошибка в случае если мы не указали порт
     if (argc < 2) {
         fprintf(stderr, "ERROR, no port provided\n");
         exit(1);
     }
     
-    // Шаг 1 - создание сокета
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
     
-    // Шаг 2 - связывание сокета с локальным адресом
     bzero((char *)&serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY; // сервер принимает подключения на все IP-адреса
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
     
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR on binding");
     
-    // Шаг 3 - ожидание подключений, размер очереди - 5
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
     
-    // Шаг 4 - извлекаем сообщение из очереди (цикл извлечения запросов на подключение)
     while (1) {
         newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
         if (newsockfd < 0)
             error("ERROR on accept");
         
         nclients++;
-        
-        // вывод сведений о клиенте
+
         printf("+[%s:%d] new connect!\n", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
         printusers();
         
@@ -133,18 +120,9 @@ int main(int argc, char *argv[]) {
 }
 
 void dostuff(int sock) {
-    int bytes_recv; // размер принятого сообщения
-    int a, b;       // переменные для функций
+    int bytes_recv; 
+    int a, b; 
     char buff[1024];
-    
-    // Отправляем приветствие и список команд
-    char *welcome_msg = "Welcome! Available commands:\n"
-                        "1. Calculator mode: enter two numbers and operation (add/sub/mul/div)\n"
-                        "2. Send file: type 'sendfile' followed by filename\n"
-                        "3. Type 'quit' to exit\n\n"
-                        "Enter 1 parameter\r\n";
-    
-    send(sock, welcome_msg, strlen(welcome_msg), 0);
     
     while (1) {
         bzero(buff, sizeof(buff));
@@ -156,33 +134,26 @@ void dostuff(int sock) {
         
         buff[bytes_recv] = '\0';
         
-        // Убираем символ новой строки в конце
         if (buff[strlen(buff)-1] == '\n') {
             buff[strlen(buff)-1] = '\0';
         }
         
-        // Проверка на quit
         if (strcmp(buff, "quit") == 0) {
-            send(sock, "Goodbye!\n", 9, 0);
             break;
         }
         
-        // Проверка на команду отправки файла
         if (strncmp(buff, "sendfile ", 9) == 0) {
             char filename[256];
             strcpy(filename, buff + 9);
             send(sock, "READY to receive file\n", 22, 0);
             receive_file(sock, filename);
             
-            // Возвращаемся в режим калькулятора
             send(sock, "File received. Enter 1 parameter\r\n", 35, 0);
             continue;
         }
-        
-        // Режим калькулятора - получаем первый параметр
+
         a = atoi(buff);
         
-        // Запрашиваем второй параметр
         send(sock, "Enter 2 parameter\r\n", 20, 0);
         
         bzero(buff, sizeof(buff));
@@ -193,7 +164,6 @@ void dostuff(int sock) {
         
         b = atoi(buff);
         
-        // Запрашиваем операцию
         send(sock, "Enter operation (add/sub/mul/div)\r\n", 35, 0);
         
         bzero(buff, sizeof(buff));
@@ -233,7 +203,7 @@ void dostuff(int sock) {
         send(sock, "Enter 1 parameter (or 'quit' to exit)\r\n", 40, 0);
     }
     
-    nclients--; // уменьшаем счетчик активных клиентов
+    nclients--;
     printf("-disconnect\n");
     printusers();
     close(sock);
